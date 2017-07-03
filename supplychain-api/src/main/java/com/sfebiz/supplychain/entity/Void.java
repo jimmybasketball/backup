394c6af40380f149e61575dccb71e05aa23f0d0e
liujunchi <liujunchi@ifunq.com>
2017-07-03 11:22:35 +0800

    实现序列化接口

--- supplychain-api/src/main/java/com/sfebiz/supplychain/entity/Void.java
+++ supplychain-api/src/main/java/com/sfebiz/supplychain/entity/Void.java
@@ -1,11 +1,15 @@
 package com.sfebiz.supplychain.entity;
 
+import java.io.Serializable;
+
 /**
  * 空对象，作为接口通用响应的泛型标识
  * 使用此对象时，通用响应对象应只包含响应码和响应消息
  * @author liujc
  * @create 2017-06-29 16:23
  **/
-public class Void {
+public class Void implements Serializable{
+    private static final long serialVersionUID = 7437613581392434572L;
+
     private Void(){}
 }
