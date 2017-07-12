ae907533b5be5c7aaff80ff6d12145f3e2086de5
mate <timmaorz@gmail.com>
2017-07-12 18:07:57 +0800

    [MDF] 仓库服务代码添加

--- supplychain-service/src/main/java/com/sfebiz/supplychain/LocalDubboBoot.java
+++ supplychain-service/src/main/java/com/sfebiz/supplychain/LocalDubboBoot.java
@@ -3,11 +3,7 @@ package com.sfebiz.supplychain;
 public class LocalDubboBoot {
 
     public static void main(String[] args) throws Exception {
-        com.alibaba.dubbo.container.Main.main(args);
-
-        //String ss = "  1 ";
-        //ss = ss.trim();
-        //System.out.println(ss);
+	com.alibaba.dubbo.container.Main.main(args);
     }
 
 }
