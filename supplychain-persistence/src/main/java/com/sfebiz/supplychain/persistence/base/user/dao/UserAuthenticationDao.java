3908b77788ad71304185a0a7533893da74a574e7
mate <timmaorz@gmail.com>
2017-07-26 16:47:56 +0800

    [ADD] 代码添加

---
+++ supplychain-persistence/src/main/java/com/sfebiz/supplychain/persistence/base/user/dao/UserAuthenticationDao.java
@@ -0,0 +1,17 @@
+package com.sfebiz.supplychain.persistence.base.user.dao;
+
+import com.sfebiz.common.dao.BaseDao;
+import com.sfebiz.supplychain.persistence.base.user.domain.UserAuthenticationDO;
+
+/**
+ * 
+ * <p>
+ * 用户实名认证dao
+ * </p>
+ * 
+ * @author matt
+ * @Date 2017年7月7日 上午10:44:37
+ */
+public interface UserAuthenticationDao extends BaseDao<UserAuthenticationDO> {
+
+}
